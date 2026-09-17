382e15a34ee7d1fbadb68fd01db69dcc2a71fc50
zuotengguiji577-hub <zuotengguiji577@gmail.com>
2026-09-17 20:10:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="softtennis-analysis-v1";
-const APP_SHELL=["./","./index.html","./manifest.webmanifest"];
+const CACHE_NAME="softtennis-analysis-v3";
+const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
